fdbe84ab95c7fffbe0d16ca63f2f57596a681947
Ingvild Lyckander <ingvild.lyckander@gmail.com>
2015-08-14 16:52:40 +0200

    Kan genereisk hente ut id i url-en og sender med svaret til admin-siden

--- public/mentometer.js
+++ public/mentometer.js
@@ -2,13 +2,12 @@
 
     var sock = new SockJS('/echo');
 
-    sock.onopen = createSend({ type: "connect", name: "ape" });
+    sock.onopen = createSend({ type: "connect", name: window.location.hash.substr(1) });
     sock.onclose = function() { location.reload() };
 
     function answer(alternative) {
         var type = window.location.hash.substr(1);
-        console.log(type);
-        return createSend({ type: 'answer', alternative: alternative, name: "ape" });
+        return createSend({ type: 'answer', alternative: alternative, name: type });
     }
 
     function createSend(data) {
@@ -21,5 +20,10 @@
     document.querySelector('.alternative-b').addEventListener('click', answer('b'));
     document.querySelector('.alternative-c').addEventListener('click', answer('c'));
     document.querySelector('.alternative-d').addEventListener('click', answer('d'));
+
+    document.getElementById('alternative-a').href = '#' + window.location.hash.substr(1);
+    document.getElementById('alternative-b').href = '#' + window.location.hash.substr(1);
+    document.getElementById('alternative-c').href = '#' + window.location.hash.substr(1);
+    document.getElementById('alternative-d').href = '#' + window.location.hash.substr(1);
 
 })();
