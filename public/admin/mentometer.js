db70b9b6c1362938b9480bf61ce9cdec9a8bb4d2
Ingvild Lyckander <ingvild.lyckander@gmail.com>
2015-08-24 13:41:39 +0200

    Kan ikke se hva de andre lagene har besvart

--- public/admin/mentometer.js
+++ public/admin/mentometer.js
@@ -1,13 +1,11 @@
 (function() {
 
     window.createMentometer = function(questions, teams) {
-        var chart,
-            currentQuestion,
+        var currentQuestion,
             answers = {},
             allowAnswers = true;
 
-        var answersEl = document.querySelector('.answers'),
-            questionNumEl = document.querySelector('.question .num'),
+        var questionNumEl = document.querySelector('.question .num'),
             questionEl = document.querySelector('.question .question-text'),
             nextQuestionEl = document.querySelector('.next-question'),
             nextQuestionLinkEl = document.querySelector('.next-question a'),
@@ -19,8 +17,6 @@
             nextQuestionEl.style['display'] = 'none';
             nextQuestionLinkEl.setAttribute('href', '#/question/' + (num + 1));
             currentQuestion = num;
-
-            answersEl.innerHTML = '';
 
             answers = { 'a': [], 'b': [], 'c': [], 'd': [] };
 
@@ -37,13 +33,6 @@
                 alternativesEl.appendChild(li);
             }
 
-            chart = answersChart({
-                el: answersEl,
-                alternatives: alternatives
-            });
-
-            chart.update(answers);
-
             var keepRunning = function(n) {
                 return function() {
                     return n === currentQuestion;
@@ -52,8 +41,7 @@
 
             function showRightAnswerAndScore() {
                 alternativesEl.innerHTML = '';
-                answersEl.innerHTML = '';
-
+                
                 teams.updateScores(question.rightanswer);
                 teams.showScore(question.rightanswer);
                 //scoretitle.innerHTML = 'Scorelist';
@@ -92,13 +80,6 @@
                 teams.showAnswers();
 
 
-
-
-
-                //upgrade users score if right answer
-
-
-                chart.update(answers);
             }
         }
     }
@@ -111,85 +92,6 @@
         return (time / 1000).toFixed(2);
     }
 
-    function answersChart(options) {
-        var margin = {top: 30, right: 0, bottom: 55, left: 0},
-            width = utils.windowWidth() - margin.left - margin.right,
-            height = 320 - margin.top - margin.bottom;
-
-        var alternatives = options.alternatives;
-
-        var x = d3.scale.ordinal()
-            .rangeRoundBands([0, width], .1);
-
-        var y = d3.scale.linear()
-            .range([height, 0]);
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .tickPadding(10)
-            .tickSize(1)
-            .orient("bottom");
-
-        var svg = d3.select(options.el)
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        x.domain(alternatives.map(function(d) { return d.alt; }));
-
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("fill", "#b9b9b9")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        svg.selectAll(".bar")
-            .data(alternatives)
-            .enter().append("rect")
-            .attr("class", function(d) { return "bar alternative-" + d.alt; })
-            .attr("x", function(d) { return x(d.answer); })
-            .attr("width", x.rangeBand());
-
-        svg.append("g").selectAll("text")
-            .data(alternatives)
-            .enter().append("text")
-            .attr("text-anchor", "middle")
-            .attr("class", "alternative-count")
-            .attr("fill", "#fff")
-            .attr("x", function(d, i) {
-                return x.range()[i] + x.rangeBand() / 2;
-            });
-
-        var update = function(answers) {
-            y.domain([0, d3.max(alternatives, function(d) { return answers[d.alt].length; })]);
-
-            svg.selectAll(".bar")
-                .data(alternatives)
-                .transition()
-                .duration(300)
-                .attr("y", function(d) { return y(answers[d.alt].length); })
-                .attr("height", function(d) { return height - y(answers[d.alt].length); });
-
-            svg.selectAll(".alternative-count")
-                .data(alternatives)
-                .transition()
-                .duration(300)
-                .text(function(d) {
-                    var len = answers[d.alt].length;
-                    return len == 0 ? '' : len;
-                })
-                .attr("y", function(d) {
-                    var max = y.domain()[1];
-                    return height - (answers[d.alt].length * height / max) - 4;
-                });
-        };
-
-        return {
-            update: utils.throttle(update, 500)
-        }
-    }
 
     function prepareAlternatives(alternatives) {
         return utils.map(alternatives, function(answer, i) {
