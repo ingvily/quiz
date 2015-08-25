06ee73f33a980e770e7cd9e9084607ac2a1b4064
Ingvild Lyckander <ingvild.lyckander@gmail.com>
2015-08-25 21:47:12 +0200

    Lagt inn alle spørsmål

--- public/admin/dashboard.js
+++ public/admin/dashboard.js
@@ -10,23 +10,165 @@
             "rightanswer": "a"
         },
         {
-            "question": "Hvilken bokstav er før den tredje i&nbsp;alfabetet?",
+            "question": "Uttrykket “sjakk matt stammer fra persisk, og betyr ...”?",
             "alternatives": [
-                "C",
-                "A",
-                "B"
+                "Kongen er beseiret",
+                "Kongen er død",
+                "Det er over",
+                "Du er død"
             ],
-            "rightanswer": "c"
+            "rightanswer": "a"
 
         },
         {
-            "question": "Hva er fremtidens&nbsp;plattform?",
+            "question": "Hva står CIA for?",
             "alternatives": [
-                "JVM",
-                "CLR",
-                "JS"
+                "Criminal investigation agency",
+                "Central Intelligence Agency",
+                "Central Investigation Agency",
+                "Civillian Intelligence Agency"
             ],
             "rightanswer": "b"
+        },
+        {
+            "question": "Hvilket grunnstoff har det kjemiske symbolet Ag?",
+            "alternatives": [
+                "Gull",
+                "Sølv",
+                "Kvittsølv",
+                "Jern"
+            ],
+            "rightanswer": "b"
+        },
+        {
+            "question": "Hvilke av disse vindstyrkene er sterkest? ",
+            "alternatives": [
+                "Laber bris",
+                "Lett bris",
+                "Flau vind",
+                "Svak vind"
+            ],
+            "rightanswer": "a"
+        },
+        {
+            "question": "Hva heter landet med dette flagget? ",
+            "alternatives": [
+                "Malaysia",
+                "Burma",
+                "Kambodsja",
+                "Thailand"
+            ],
+            "rightanswer": "c"
+        },
+        {
+            "question": "Hva heter kunnskapsminister vår? ",
+            "alternatives": [
+                "Bent Høie",
+                "Børge Brende",
+                "Bård Vegar Solhjell",
+                "Torbjørn Røe Isaksen"
+            ],
+            "rightanswer": "d"
+        },
+        {
+            "question": "Hvilket år ble BEKK stiftet? ",
+            "alternatives": [
+                "2000",
+                "2001",
+                "2002",
+                "2003"
+            ],
+            "rightanswer": "a"
+        },
+        {
+            "question": "Hvem av disse er sjefen i BEKK?",
+            "alternatives": [
+                "Bernt",
+                "frode",
+                "fredrik",
+                "olav"
+            ],
+            "rightanswer": "d"
+        },
+        {
+            "question": "Hva er kallenavnet på BEKK sine to kontorer?",
+            "alternatives": [
+                "Brakka og Festningen",
+                "Skuret og Brakka",
+                "Blikkboksen og Skuret",
+                "Blikkboksen og Festningen"
+            ],
+            "rightanswer": "c"
+        },
+        {
+            "question": "Hvor mange faggrupper er det i BEKK? ",
+            "alternatives": [
+                "40",
+                "30",
+                "20",
+                "15"
+            ],
+            "rightanswer": "b"
+        },
+        {
+            "question": "I fjor vant BEKK designprisen Merket for god design, for hvilken kunde var det?",
+            "alternatives": [
+                "Sparebank1",
+                "Statens vegvesen",
+                "Posten",
+                "Den Norske Opera & Ballett"
+            ],
+            "rightanswer": "d"
+        },
+        {
+            "question": "Hva skjedde på innflytningsfesten til BEKK?",
+            "alternatives": [
+                "Gulvet knakk sammen",
+                "Gulvet knakk sammen",
+                "Gulvet knakk sammen",
+                "Gulvet knakk sammen"
+            ],
+            "rightanswer": "d"
+        },
+        {
+            "question": "Hvor mange liter øl er det i en pint øl?",
+            "alternatives": [
+                "ca. 0,473",
+                "0,5",
+                "ca. 0,563",
+                "ca. 0,673"
+            ],
+            "rightanswer": "a"
+        },
+        {
+            "question": "Hvilke av disse alternativene er fri for gluten?",
+            "alternatives": [
+                "bilde",
+                "bilde",
+                "bilde",
+                "bilde"
+            ],
+            "rightanswer": "a"
+        },
+        {
+            "question": "Hva er det som gjør at rødvinen er rød og hvitvinen er hvit?",
+            "alternatives": [
+                "Rød/grønne druer",
+                "Over-/undergjæring",
+                "Sukkertype",
+                "Med/uten skall"
+            ],
+            "rightanswer": "d"
+        },
+        {
+            "question": "Hvilke av disse har flest/færrest kcal per 100 gram?",
+            "alternatives": [
+                "bilde",
+                "bilde",
+                "bilde",
+                "bilde"
+            ],
+            "rightanswer": "a"
         }
     ];
 
